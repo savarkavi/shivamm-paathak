70ebc22811dcae6fc525087fb16fe9447c8fea9b
Sushant Sharma <sushant20.sharma00@gmail.com>
2026-02-01 08:38:24 +0530

    add dummy featuredProjetsData in constants file

---
+++ src/lib/constants.ts
@@ -0,0 +1,35 @@
+export const featuredProjectsData = [
+  {
+    id: "01",
+    brand: "estrella",
+    category: "fashion",
+    type: "photo",
+    files: 3,
+    year: 2025,
+  },
+  {
+    id: "02",
+    brand: "gauri & nainika",
+    category: "luxury",
+    type: "photo",
+
+    files: 2,
+    year: 2023,
+  },
+  {
+    id: "03",
+    brand: "july issue",
+    category: "commercial",
+    type: "film",
+    files: 6,
+    year: 2026,
+  },
+  {
+    id: "04",
+    brand: "raabta by rahul",
+    category: "commercial",
+    type: "photo",
+    files: 4,
+    year: 2025,
+  },
+];
