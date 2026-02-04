ff998a4082c8ea318e7f7568b1e4663404cc70c2
Sushant Sharma <sushant20.sharma00@gmail.com>
2026-02-04 08:57:54 +0530

    refactor featuredProjectsData to categoriesData with detailed project structure

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,35 +1,654 @@
-export const featuredProjectsData = [
+export const categoriesData = [
   {
     id: "01",
-    brand: "estrella",
-    category: "fashion",
+    category: "Commercial",
     type: "photo",
-    files: 3,
-    year: 2025,
+    projects: [
+      {
+        info: [
+          { label: "brand", value: "estrella" },
+          { label: "files", value: "03" },
+          { label: "year", value: "2025" },
+        ],
+        credits: [
+          {
+            label: "Creative direction & photography",
+            value: "Shivamm Paathak",
+          },
+          {
+            label: "Talents",
+            value: "Castille",
+          },
+          {
+            label: "Styling",
+            value: "Sonali Khulla",
+          },
+          {
+            label: "Assist",
+            value: "Ishika",
+          },
+          {
+            label: "Hair & makeup",
+            value: "Akash pant",
+          },
+        ],
+        imgSrc:
+          "https://res.cloudinary.com/dcsv0xhjz/image/upload/v1769937174/shivamm-paathak/commercial/Shot_xhoriw_hoorvi.j.valaya_shivamm_paathak_makeupsbysrishtianand._Shoot_jewelry_photogra_fffyax.jpg",
+      },
+      {
+        info: [
+          { label: "brand", value: "estrella" },
+          { label: "files", value: "03" },
+          { label: "year", value: "2025" },
+        ],
+        credits: [
+          {
+            label: "Creative direction & photography",
+            value: "Shivamm Paathak",
+          },
+          {
+            label: "Talents",
+            value: "Castille",
+          },
+          {
+            label: "Styling",
+            value: "Sonali Khulla",
+          },
+          {
+            label: "Assist",
+            value: "Ishika",
+          },
+          {
+            label: "Hair & makeup",
+            value: "Akash pant",
+          },
+        ],
+        imgSrc:
+          "https://res.cloudinary.com/dcsv0xhjz/image/upload/v1769937128/shivamm-paathak/commercial/r.elan.official_circulardesignchallenge_uninindia_crclestudio_farakwear_siddhantsharma779_sl5d3t.jpg",
+      },
+      {
+        info: [
+          { label: "brand", value: "estrella" },
+          { label: "files", value: "03" },
+          { label: "year", value: "2025" },
+        ],
+        credits: [
+          {
+            label: "Creative direction & photography",
+            value: "Shivamm Paathak",
+          },
+          {
+            label: "Talents",
+            value: "Castille",
+          },
+          {
+            label: "Styling",
+            value: "Sonali Khulla",
+          },
+          {
+            label: "Assist",
+            value: "Ishika",
+          },
+          {
+            label: "Hair & makeup",
+            value: "Akash pant",
+          },
+        ],
+        imgSrc:
+          "https://res.cloudinary.com/dcsv0xhjz/image/upload/v1769937128/shivamm-paathak/commercial/Brought_to_life_the_coziest_Christmas_atmosphere_just_for_fraganote_Photography_visual_direct_l8tbg7.jpg",
+      },
+      {
+        info: [
+          { label: "brand", value: "estrella" },
+          { label: "files", value: "03" },
+          { label: "year", value: "2025" },
+        ],
+        credits: [
+          {
+            label: "Creative direction & photography",
+            value: "Shivamm Paathak",
+          },
+          {
+            label: "Talents",
+            value: "Castille",
+          },
+          {
+            label: "Styling",
+            value: "Sonali Khulla",
+          },
+          {
+            label: "Assist",
+            value: "Ishika",
+          },
+          {
+            label: "Hair & makeup",
+            value: "Akash pant",
+          },
+        ],
+        imgSrc:
+          "https://res.cloudinary.com/dcsv0xhjz/image/upload/v1769937037/shivamm-paathak/commercial/Talents_-_lluizamouraa_estrellamgmt_Film_edit_shivamm_paathak_Creative_direction_Photograp_jiqyoi.jpg",
+      },
+      {
+        info: [
+          { label: "brand", value: "estrella" },
+          { label: "files", value: "03" },
+          { label: "year", value: "2025" },
+        ],
+        credits: [
+          {
+            label: "Creative direction & photography",
+            value: "Shivamm Paathak",
+          },
+          {
+            label: "Talents",
+            value: "Castille",
+          },
+          {
+            label: "Styling",
+            value: "Sonali Khulla",
+          },
+          {
+            label: "Assist",
+            value: "Ishika",
+          },
+          {
+            label: "Hair & makeup",
+            value: "Akash pant",
+          },
+        ],
+        imgSrc:
+          "https://res.cloudinary.com/dcsv0xhjz/image/upload/v1769918479/shivamm-paathak/commercial/Where_light_is_sacred._Where_beauty_is_unfamiliar_An_Alien_Christmas._Welcome_to_my_world_s_exsgc4.jpg",
+      },
+    ],
+    totalProjects: "12",
   },
   {
     id: "02",
-    brand: "gauri & nainika",
     category: "luxury",
     type: "photo",
-
-    files: 2,
-    year: 2023,
+    projects: [
+      {
+        info: [
+          { label: "brand", value: "estrella" },
+          { label: "files", value: "03" },
+          { label: "year", value: "2025" },
+        ],
+        credits: [
+          {
+            label: "Creative direction & photography",
+            value: "Shivamm Paathak",
+          },
+          {
+            label: "Talents",
+            value: "Castille",
+          },
+          {
+            label: "Styling",
+            value: "Sonali Khulla",
+          },
+          {
+            label: "Assist",
+            value: "Ishika",
+          },
+          {
+            label: "Hair & makeup",
+            value: "Akash pant",
+          },
+        ],
+        imgSrc:
+          "https://res.cloudinary.com/dcsv0xhjz/image/upload/v1769937174/shivamm-paathak/commercial/Shot_xhoriw_hoorvi.j.valaya_shivamm_paathak_makeupsbysrishtianand._Shoot_jewelry_photogra_fffyax.jpg",
+      },
+      {
+        info: [
+          { label: "brand", value: "estrella" },
+          { label: "files", value: "03" },
+          { label: "year", value: "2025" },
+        ],
+        credits: [
+          {
+            label: "Creative direction & photography",
+            value: "Shivamm Paathak",
+          },
+          {
+            label: "Talents",
+            value: "Castille",
+          },
+          {
+            label: "Styling",
+            value: "Sonali Khulla",
+          },
+          {
+            label: "Assist",
+            value: "Ishika",
+          },
+          {
+            label: "Hair & makeup",
+            value: "Akash pant",
+          },
+        ],
+        imgSrc:
+          "https://res.cloudinary.com/dcsv0xhjz/image/upload/v1769937128/shivamm-paathak/commercial/r.elan.official_circulardesignchallenge_uninindia_crclestudio_farakwear_siddhantsharma779_sl5d3t.jpg",
+      },
+      {
+        info: [
+          { label: "brand", value: "estrella" },
+          { label: "files", value: "03" },
+          { label: "year", value: "2025" },
+        ],
+        credits: [
+          {
+            label: "Creative direction & photography",
+            value: "Shivamm Paathak",
+          },
+          {
+            label: "Talents",
+            value: "Castille",
+          },
+          {
+            label: "Styling",
+            value: "Sonali Khulla",
+          },
+          {
+            label: "Assist",
+            value: "Ishika",
+          },
+          {
+            label: "Hair & makeup",
+            value: "Akash pant",
+          },
+        ],
+        imgSrc:
+          "https://res.cloudinary.com/dcsv0xhjz/image/upload/v1769937128/shivamm-paathak/commercial/Brought_to_life_the_coziest_Christmas_atmosphere_just_for_fraganote_Photography_visual_direct_l8tbg7.jpg",
+      },
+      {
+        info: [
+          { label: "brand", value: "estrella" },
+          { label: "files", value: "03" },
+          { label: "year", value: "2025" },
+        ],
+        credits: [
+          {
+            label: "Creative direction & photography",
+            value: "Shivamm Paathak",
+          },
+          {
+            label: "Talents",
+            value: "Castille",
+          },
+          {
+            label: "Styling",
+            value: "Sonali Khulla",
+          },
+          {
+            label: "Assist",
+            value: "Ishika",
+          },
+          {
+            label: "Hair & makeup",
+            value: "Akash pant",
+          },
+        ],
+        imgSrc:
+          "https://res.cloudinary.com/dcsv0xhjz/image/upload/v1769937037/shivamm-paathak/commercial/Talents_-_lluizamouraa_estrellamgmt_Film_edit_shivamm_paathak_Creative_direction_Photograp_jiqyoi.jpg",
+      },
+      {
+        info: [
+          { label: "brand", value: "estrella" },
+          { label: "files", value: "03" },
+          { label: "year", value: "2025" },
+        ],
+        credits: [
+          {
+            label: "Creative direction & photography",
+            value: "Shivamm Paathak",
+          },
+          {
+            label: "Talents",
+            value: "Castille",
+          },
+          {
+            label: "Styling",
+            value: "Sonali Khulla",
+          },
+          {
+            label: "Assist",
+            value: "Ishika",
+          },
+          {
+            label: "Hair & makeup",
+            value: "Akash pant",
+          },
+        ],
+        imgSrc:
+          "https://res.cloudinary.com/dcsv0xhjz/image/upload/v1769918479/shivamm-paathak/commercial/Where_light_is_sacred._Where_beauty_is_unfamiliar_An_Alien_Christmas._Welcome_to_my_world_s_exsgc4.jpg",
+      },
+    ],
+    totalProjects: "16",
   },
   {
     id: "03",
-    brand: "july issue",
-    category: "commercial",
-    type: "film",
-    files: 6,
-    year: 2026,
+    category: "Editorial",
+    type: "photo",
+    projects: [
+      {
+        info: [
+          { label: "brand", value: "estrella" },
+          { label: "files", value: "03" },
+          { label: "year", value: "2025" },
+        ],
+        credits: [
+          {
+            label: "Creative direction & photography",
+            value: "Shivamm Paathak",
+          },
+          {
+            label: "Talents",
+            value: "Castille",
+          },
+          {
+            label: "Styling",
+            value: "Sonali Khulla",
+          },
+          {
+            label: "Assist",
+            value: "Ishika",
+          },
+          {
+            label: "Hair & makeup",
+            value: "Akash pant",
+          },
+        ],
+        imgSrc:
+          "https://res.cloudinary.com/dcsv0xhjz/image/upload/v1769937174/shivamm-paathak/commercial/Shot_xhoriw_hoorvi.j.valaya_shivamm_paathak_makeupsbysrishtianand._Shoot_jewelry_photogra_fffyax.jpg",
+      },
+      {
+        info: [
+          { label: "brand", value: "estrella" },
+          { label: "files", value: "03" },
+          { label: "year", value: "2025" },
+        ],
+        credits: [
+          {
+            label: "Creative direction & photography",
+            value: "Shivamm Paathak",
+          },
+          {
+            label: "Talents",
+            value: "Castille",
+          },
+          {
+            label: "Styling",
+            value: "Sonali Khulla",
+          },
+          {
+            label: "Assist",
+            value: "Ishika",
+          },
+          {
+            label: "Hair & makeup",
+            value: "Akash pant",
+          },
+        ],
+        imgSrc:
+          "https://res.cloudinary.com/dcsv0xhjz/image/upload/v1769937128/shivamm-paathak/commercial/r.elan.official_circulardesignchallenge_uninindia_crclestudio_farakwear_siddhantsharma779_sl5d3t.jpg",
+      },
+      {
+        info: [
+          { label: "brand", value: "estrella" },
+          { label: "files", value: "03" },
+          { label: "year", value: "2025" },
+        ],
+        credits: [
+          {
+            label: "Creative direction & photography",
+            value: "Shivamm Paathak",
+          },
+          {
+            label: "Talents",
+            value: "Castille",
+          },
+          {
+            label: "Styling",
+            value: "Sonali Khulla",
+          },
+          {
+            label: "Assist",
+            value: "Ishika",
+          },
+          {
+            label: "Hair & makeup",
+            value: "Akash pant",
+          },
+        ],
+        imgSrc:
+          "https://res.cloudinary.com/dcsv0xhjz/image/upload/v1769937128/shivamm-paathak/commercial/Brought_to_life_the_coziest_Christmas_atmosphere_just_for_fraganote_Photography_visual_direct_l8tbg7.jpg",
+      },
+      {
+        info: [
+          { label: "brand", value: "estrella" },
+          { label: "files", value: "03" },
+          { label: "year", value: "2025" },
+        ],
+        credits: [
+          {
+            label: "Creative direction & photography",
+            value: "Shivamm Paathak",
+          },
+          {
+            label: "Talents",
+            value: "Castille",
+          },
+          {
+            label: "Styling",
+            value: "Sonali Khulla",
+          },
+          {
+            label: "Assist",
+            value: "Ishika",
+          },
+          {
+            label: "Hair & makeup",
+            value: "Akash pant",
+          },
+        ],
+        imgSrc:
+          "https://res.cloudinary.com/dcsv0xhjz/image/upload/v1769937037/shivamm-paathak/commercial/Talents_-_lluizamouraa_estrellamgmt_Film_edit_shivamm_paathak_Creative_direction_Photograp_jiqyoi.jpg",
+      },
+      {
+        info: [
+          { label: "brand", value: "estrella" },
+          { label: "files", value: "03" },
+          { label: "year", value: "2025" },
+        ],
+        credits: [
+          {
+            label: "Creative direction & photography",
+            value: "Shivamm Paathak",
+          },
+          {
+            label: "Talents",
+            value: "Castille",
+          },
+          {
+            label: "Styling",
+            value: "Sonali Khulla",
+          },
+          {
+            label: "Assist",
+            value: "Ishika",
+          },
+          {
+            label: "Hair & makeup",
+            value: "Akash pant",
+          },
+        ],
+        imgSrc:
+          "https://res.cloudinary.com/dcsv0xhjz/image/upload/v1769918479/shivamm-paathak/commercial/Where_light_is_sacred._Where_beauty_is_unfamiliar_An_Alien_Christmas._Welcome_to_my_world_s_exsgc4.jpg",
+      },
+    ],
+    totalProjects: "11",
   },
   {
     id: "04",
-    brand: "raabta by rahul",
-    category: "commercial",
-    type: "photo",
-    files: 4,
-    year: 2025,
+    category: "Music Videos",
+    type: "Film",
+    projects: [
+      {
+        info: [
+          { label: "brand", value: "estrella" },
+          { label: "files", value: "03" },
+          { label: "year", value: "2025" },
+        ],
+        credits: [
+          {
+            label: "Creative direction & photography",
+            value: "Shivamm Paathak",
+          },
+          {
+            label: "Talents",
+            value: "Castille",
+          },
+          {
+            label: "Styling",
+            value: "Sonali Khulla",
+          },
+          {
+            label: "Assist",
+            value: "Ishika",
+          },
+          {
+            label: "Hair & makeup",
+            value: "Akash pant",
+          },
+        ],
+        imgSrc:
+          "https://res.cloudinary.com/dcsv0xhjz/image/upload/v1769937174/shivamm-paathak/commercial/Shot_xhoriw_hoorvi.j.valaya_shivamm_paathak_makeupsbysrishtianand._Shoot_jewelry_photogra_fffyax.jpg",
+      },
+      {
+        info: [
+          { label: "brand", value: "estrella" },
+          { label: "files", value: "03" },
+          { label: "year", value: "2025" },
+        ],
+        credits: [
+          {
+            label: "Creative direction & photography",
+            value: "Shivamm Paathak",
+          },
+          {
+            label: "Talents",
+            value: "Castille",
+          },
+          {
+            label: "Styling",
+            value: "Sonali Khulla",
+          },
+          {
+            label: "Assist",
+            value: "Ishika",
+          },
+          {
+            label: "Hair & makeup",
+            value: "Akash pant",
+          },
+        ],
+        imgSrc:
+          "https://res.cloudinary.com/dcsv0xhjz/image/upload/v1769937128/shivamm-paathak/commercial/r.elan.official_circulardesignchallenge_uninindia_crclestudio_farakwear_siddhantsharma779_sl5d3t.jpg",
+      },
+      {
+        info: [
+          { label: "brand", value: "estrella" },
+          { label: "files", value: "03" },
+          { label: "year", value: "2025" },
+        ],
+        credits: [
+          {
+            label: "Creative direction & photography",
+            value: "Shivamm Paathak",
+          },
+          {
+            label: "Talents",
+            value: "Castille",
+          },
+          {
+            label: "Styling",
+            value: "Sonali Khulla",
+          },
+          {
+            label: "Assist",
+            value: "Ishika",
+          },
+          {
+            label: "Hair & makeup",
+            value: "Akash pant",
+          },
+        ],
+        imgSrc:
+          "https://res.cloudinary.com/dcsv0xhjz/image/upload/v1769937128/shivamm-paathak/commercial/Brought_to_life_the_coziest_Christmas_atmosphere_just_for_fraganote_Photography_visual_direct_l8tbg7.jpg",
+      },
+      {
+        info: [
+          { label: "brand", value: "estrella" },
+          { label: "files", value: "03" },
+          { label: "year", value: "2025" },
+        ],
+        credits: [
+          {
+            label: "Creative direction & photography",
+            value: "Shivamm Paathak",
+          },
+          {
+            label: "Talents",
+            value: "Castille",
+          },
+          {
+            label: "Styling",
+            value: "Sonali Khulla",
+          },
+          {
+            label: "Assist",
+            value: "Ishika",
+          },
+          {
+            label: "Hair & makeup",
+            value: "Akash pant",
+          },
+        ],
+        imgSrc:
+          "https://res.cloudinary.com/dcsv0xhjz/image/upload/v1769937037/shivamm-paathak/commercial/Talents_-_lluizamouraa_estrellamgmt_Film_edit_shivamm_paathak_Creative_direction_Photograp_jiqyoi.jpg",
+      },
+      {
+        info: [
+          { label: "brand", value: "estrella" },
+          { label: "files", value: "03" },
+          { label: "year", value: "2025" },
+        ],
+        credits: [
+          {
+            label: "Creative direction & photography",
+            value: "Shivamm Paathak",
+          },
+          {
+            label: "Talents",
+            value: "Castille",
+          },
+          {
+            label: "Styling",
+            value: "Sonali Khulla",
+          },
+          {
+            label: "Assist",
+            value: "Ishika",
+          },
+          {
+            label: "Hair & makeup",
+            value: "Akash pant",
+          },
+        ],
+        imgSrc:
+          "https://res.cloudinary.com/dcsv0xhjz/image/upload/v1769918479/shivamm-paathak/commercial/Where_light_is_sacred._Where_beauty_is_unfamiliar_An_Alien_Christmas._Welcome_to_my_world_s_exsgc4.jpg",
+      },
+    ],
+    totalProjects: "04",
   },
 ];
