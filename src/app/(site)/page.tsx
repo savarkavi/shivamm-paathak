7e022c2ffe42384809dd1a37f2400fce117ee151
Sushant Sharma <sushant20.sharma00@gmail.com>
2026-02-21 14:55:48 +0530

    add grainy effect to home page

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -7,12 +7,23 @@ import { CategoryProvider } from "@/providers/category-context";
 export default function Home() {
   return (
     <LoadingOverlay>
-      <div className="hero-container flex h-screen w-full flex-col justify-between overflow-hidden bg-black">
-        <CategoryProvider>
-          <Header />
-          <ProjectGallery />
-          <CategoriesContainer />
-        </CategoryProvider>
+      <div>
+        <div
+          className="fixed top-0 left-0 z-10 h-screen w-screen"
+          style={{
+            backgroundImage: "url('/grainy-effect.webp')",
+            mixBlendMode: "hard-light",
+            opacity: 0.06,
+            pointerEvents: "none",
+          }}
+        />
+        <div className="hero-container flex h-screen w-full flex-col justify-between overflow-hidden bg-stone-950">
+          <CategoryProvider>
+            <Header />
+            <ProjectGallery />
+            <CategoriesContainer />
+          </CategoryProvider>
+        </div>
       </div>
     </LoadingOverlay>
   );
